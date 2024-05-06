198548c69f9ac11ea23ca4ffb42947cc7f45a884
sosthene14 <sosthenemounsambote14@gmail.com>
2024-05-06 13:36:15 +0000

    updated some code

--- src/components/ts/SendPictures.ts
+++ src/components/ts/SendPictures.ts
@@ -10,7 +10,7 @@ export const sendData = async (files: File[],senderName:string) : Promise<respon
         formData.append("senderName", senderName);
         formData.append(`images[${index}]`, file);
     });
-    fetch("http://127.0.0.1:5000/upload", {
+    fetch("https://rostel-high-tech-concours-photo-backend.vercel.app/upload", {
         method: "POST",
         body: formData,
     })
